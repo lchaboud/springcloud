dbb422ad93913817f402b4abade9c7855e091f6a
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-11 15:45:34 +0100

    client discovery

--- eureka/src/main/java/org/demo/Application.java
+++ eureka/src/main/java/org/demo/Application.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
